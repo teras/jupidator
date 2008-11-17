7b5dbcdb1c8421a1ca85d04e93f9b90c26563772
teras <devnull@localhost>
2008-11-17 03:24:35 +0200

    [svn r20] supporting simple CSS styles for version entry, instead of tables

--- src/com/panayotis/jupidator/html/DefaultHTMLCreator.java
+++ src/com/panayotis/jupidator/html/DefaultHTMLCreator.java
@@ -3,7 +3,6 @@
  * and open the template in the editor.
  */
 package com.panayotis.jupidator.html;
-import java.awt.Color;
 import static com.panayotis.jupidator.i18n.I18N._;
 
 /**
@@ -13,33 +12,32 @@ import static com.panayotis.jupidator.i18n.I18N._;
 public class DefaultHTMLCreator implements UpdaterHTMLCreator {
 
     private StringBuffer data;
-    private String backcolor;
 
     public DefaultHTMLCreator() {
         data = new StringBuffer();
         data.append("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\n");
         data.append("<html>\n");
-        data.append("<head><title></title></head>\n");
+        data.append("<head>\n");
+        data.append("<title></title>\n");
+        data.append("<style type=\"text/css\">\n");
+        data.append(".version { padding:4px 4px 4px 4px; margin: 5px 10px 5px 10px; background: #d2e6d2; font-weight: bold; }\n");
+        data.append(".info { margin: 0px 10px 16px 12px; }\n");
+        data.append("</style>\n");
+        data.append("</head>\n");
         data.append("<body>\n");
-        setHeaderBackColor(new Color(210, 210, 230));
     }
 
     public void addInfo(String lastrelease, String information) {
-        data.append("<table style=\"text-align: left; margin-top: 6px; margin-left: 10px; margin-right: 10px;\" border=\"0\" cellpadding=\"4\">\n");
-        data.append("<tr><td style=\"background-color: "+backcolor+";\"><span style=\"font-weight: bold;\">");
+        data.append("<div class=\"version\">");
         data.append(_("Version"));
         data.append(": ").append(lastrelease);
-        data.append("</span></td></tr>\n<tr><td>");
+        data.append("</div>\n<div class=\"info\">");
         data.append(information);
-        data.append("</td></tr>\n");
-        data.append("</table><br>\n");
+        data.append("</div>\n");
     }
 
     public String getHTML() {
         return data.toString() + "</body>\n</html>\n";
     }
     
-    public void setHeaderBackColor(Color c) {
-        backcolor = "rgb("+c.getRed()+", "+c.getBlue()+", "+c.getGreen()+")";
-    }
 }
